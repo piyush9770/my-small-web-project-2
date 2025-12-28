85d9edd0f673aa3cbdb1e9dd03247b6b0db33baf
piyush verma <piyush@example.com>
2025-12-28 16:41:51 +0530

    Fix API URL to new backend service

--- client/js/config.js
+++ client/js/config.js
@@ -1,4 +1,4 @@
 // Auto detect environment
 const API = window.location.hostname === "localhost"
   ? "http://localhost:5000/api"
-  : "https://my-small-web-project-2.onrender.com/api";
+  : "https://my-small-web-project-2-backend.onrender.com/api";
